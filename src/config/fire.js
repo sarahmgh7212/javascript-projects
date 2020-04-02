d2e704e9ba69e8fac8b0c1434f5fd10f9f117c43
sarahmgh7212 <50006941+sarahmgh7212@users.noreply.github.com>
2020-04-02 17:07:42 +1100

    removed api key

--- src/config/fire.js
+++ src/config/fire.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
   // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyACsWMm0WD0C2k95r9Zi5DLDfb0DHF9H1o",
+    apiKey: "API Key",
     authDomain: "one-receipt-retailer.firebaseapp.com",
     databaseURL: "https://one-receipt-retailer.firebaseio.com",
     projectId: "one-receipt-retailer",
     storageBucket: "one-receipt-retailer.appspot.com",
-    messagingSenderId: "815446679807",
-    appId: "1:815446679807:web:6149194d99db39571d6cca",
+    messagingSenderId: " messagingSenderId",
+    appId: "app ID",
     measurementId: "G-8S1F1HXDDQ"
   };
   // Initialize Firebase
